0b3ca1909a5ca6607364bc34ac8f88d508375021
Andris Horvath <horvath.andras216@gmail.com>
2025-08-08 00:45:03 +0100

    setup maven central publishing for the gradle plugin

--- l10n-gradle-plugin/build.gradle.kts
+++ l10n-gradle-plugin/build.gradle.kts
@@ -1,7 +1,9 @@
 plugins {
     `java-gradle-plugin`
-    alias(libs.plugins.gradle.plugin.publish)
+    `maven-publish`
+    signing
     alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.jreleaser)
 }
 
 dependencyLocking {
@@ -10,7 +12,6 @@ dependencyLocking {
 }
 
 repositories {
-    // Use Maven Central for resolving dependencies.
     mavenCentral()
 }
 
@@ -36,11 +37,17 @@ testing {
     }
 }
 
+gradlePlugin.testSourceSets.add(sourceSets["functionalTest"])
+
+tasks.named<Task>("check") {
+    dependsOn(testing.suites.named("functionalTest"))
+}
+
 gradlePlugin {
     val l10n by plugins.creating {
         id = "io.github.horvathandris.localisation"
         implementationClass = "io.github.horvathandris.localisation.L10nPlugin"
-        displayName = "Localisation CodeGen"
+        displayName = "Localisation CodeGen Plugin"
         description = "Plugin for generating aergonomic code from translation message keys."
         website = "https://github.com/horvathandris/l10n"
         vcsUrl = "https://github.com/horvathandris/l10n"
@@ -52,8 +59,78 @@ gradlePlugin {
     }
 }
 
-gradlePlugin.testSourceSets.add(sourceSets["functionalTest"])
+tasks.register<Jar>("sourcesJar") {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
 
-tasks.named<Task>("check") {
-    dependsOn(testing.suites.named("functionalTest"))
+tasks.register<Jar>("javadocJar") {
+    archiveClassifier.set("javadoc")
+    from(tasks.named("javadoc"))
+}
+
+fun MavenPublication.defaultPom() = pom {
+    name = "Localisation CodeGen Plugin"
+    description = "Plugin for generating aergonomic code from translation message keys."
+    url = "https://github.com/horvathandris/l10n"
+    licenses {
+        license {
+            name = "Apache License 2.0"
+            url = "https://github.com/horvathandris/l10n/blob/main/LICENSE"
+        }
+    }
+    developers {
+        developer {
+            id = "horvathandris"
+            name = "Andris Horváth"
+        }
+    }
+    scm {
+        connection = "scm:git:https://github.com/horvathandris/l10n.git"
+        developerConnection = "scm:git:ssh://github.com/horvathandris/l10n.git"
+        url = "http://github.com/horvathandris/l10n"
+    }
+}
+
+publishing {
+    publications {
+        withType<MavenPublication> {
+            getByName("pluginMaven") {
+                artifact(tasks.named("sourcesJar"))
+                artifact(tasks.named("javadocJar"))
+            }
+            all {
+                defaultPom()
+            }
+        }
+    }
+    repositories {
+        maven {
+            url = layout.buildDirectory.dir("staging-deploy").get().asFile.toURI()
+        }
+    }
+}
+
+jreleaser {
+    signing {
+        setActive("ALWAYS")
+        armored = true
+    }
+    deploy {
+        maven {
+            mavenCentral {
+                create("sonatype") {
+                    setActive("ALWAYS")
+                    url = "https://central.sonatype.com/api/v1/publisher"
+                    stagingRepository("build/staging-deploy")
+                    gitRootSearch = true
+                }
+            }
+        }
+    }
+}
+
+val signTasks: List<Sign> = signing.sign(publishing.publications)
+tasks.withType<PublishToMavenRepository>().configureEach {
+    dependsOn(signTasks)
 }
