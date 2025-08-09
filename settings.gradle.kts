54378c9cb76ab27d7bc84db8c2e5c3bc4c5b18b0
Horváth Andris <horvath.andris@wise.com>
2025-08-09 16:45:54 +0100

    nit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,11 @@
 rootProject.name = "localisation"
 
+// Core library
 includeBuild("l10n-gradle-plugin")
 include("l10n-spring")
 include("l10n-spring-boot-starter")
+
+// Examples
 include("spring-boot-example")
 include("simple-example")
 include("groovy-dsl-example")
